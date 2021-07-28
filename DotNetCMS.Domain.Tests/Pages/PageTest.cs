ebc7778788c4e9ed4fe951b0ebfef065b59f85c8
Daniel Rotter <daniel.rotter@gmail.com>
2021-07-28 11:44:42 +0200

    Add page object with title property

---
+++ DotNetCMS.Domain.Tests/Pages/PageTest.cs
@@ -0,0 +1,16 @@
+using DotNetCMS.Domain.Pages;
+using Xunit;
+
+namespace DotNetCMS.Domain.Tests.Pages
+{
+	public class PageTest
+	{
+		[Theory]
+		[InlineData("Page Title 1"), InlineData("Page Title 2")]
+		public void ConstructWithTitle(string title)
+		{
+			var page = new Page(title);
+			Assert.Equal(title, page.Title);
+		}
+	}
+}
