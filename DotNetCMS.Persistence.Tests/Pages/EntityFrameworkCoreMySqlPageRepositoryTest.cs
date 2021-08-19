f09bef87e23ade41395387f7ff98ebd9c14efc51
Daniel Rotter <daniel.rotter@gmail.com>
2021-08-19 19:10:19 +0200

    Update github actions workflow to provide a MySQL database

--- DotNetCMS.Persistence.Tests/Pages/EntityFrameworkCoreMySqlPageRepositoryTest.cs
+++ DotNetCMS.Persistence.Tests/Pages/EntityFrameworkCoreMySqlPageRepositoryTest.cs
@@ -1,11 +1,15 @@
 using DotNetCMS.Persistence.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace DotNetCMS.Persistence.Test.Pages
 {
 	public sealed class EntityFrameworkCoreMySqlPageRepositoryTest : EntityFrameworkCorePageRepositoryTest
 	{
-		private static string _connectionString = "server=localhost;user=root;database=DotNetCMS_Test";
+		private static string _connectionString =
+			$"server=localhost;user={Environment.GetEnvironmentVariable("MYSQL_USER") ?? "root"};"
+			+ $"password={Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? ""};"
+			+ $"database={Environment.GetEnvironmentVariable("MYSQL_DATABASE") ?? "DotNetCMS_Test"}";
 
 		public EntityFrameworkCoreMySqlPageRepositoryTest()
 			: base(
